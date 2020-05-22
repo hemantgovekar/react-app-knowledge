ed70f9db8e8a7805f90f643aaac27bc9e64cd695
Hemant Govekar <hemantgovekar@gmail.com>
2020-05-22 14:51:56 +0530

    Navigation changes

--- src/component/GitSearch.js
+++ src/component/GitSearch.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Button } from 'react-bootstrap';
 
 export default function GitSearch() {
+    const [users, setUser] = useState([]);
+
+    const getUsers = async () => {
+        const randomUsers = await fetch('https://randomuser.me/api');
+        const randomUsersJson = await randomUsers.json();
+        console.log(randomUsersJson.results);
+    }
+
     return (
-        <div>
-            This is Git Search
-        </div>
+        <Button onClick={getUsers}>Search User</Button>
+        
     )
 }
--- src/component/Navigation.js
+++ src/component/Navigation.js
@@ -5,16 +5,16 @@ export default function Navigation() {
     return (
         <div>
             <Navbar bg="dark" variant="dark">
-                <Navbar.Brand href="#home">Allstate Canada</Navbar.Brand>
+                <Navbar.Brand href="/">Allstate Canada</Navbar.Brand>
                 <Nav className="mr-auto">
                     <Nav.Link  href="/">Home</Nav.Link>
                     <Nav.Link href="/aboutus">About Us</Nav.Link>
                     <Nav.Link href="/gitsearch">Git Search</Nav.Link>
                 </Nav>
-                <Form inline>
+                {/* <Form inline>
                     <FormControl type="text" placeholder="Search" className="mr-sm-2" />
                     <Button variant="outline-info">Search</Button>
-                </Form>
+                </Form> */}
             </Navbar>
         </div>
     )
